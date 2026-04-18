592ccb755e18a2b140a0f1e6e8e3561d45fd3ab7
Hygor Silva <hygorsilva404@gmail.com>
2026-04-18 20:36:17 -0300

    atualizações

---
+++ src/main/kotlin/LACOS.kt
@@ -0,0 +1,28 @@
+fun main(){
+    //Intervalos
+    /*
+    //para criar um intervalo use '..'
+    val faces = 1..6
+    //se o intervalo não incluir o valor final usar '..<'
+    val unidades = 1..<4
+    //para uma ordem inversa usar o downTo
+    val caixas = 4 downTo 1
+    //para incrementar um valor ao intervalo usar o step
+    val pulos = 1..5 step 2 //vai de 1 a 5 pulando a cada 2
+    // também ser com o char - no caso para letras
+    */
+    //Laços
+    //For
+    for (num in 1..10){
+        print("$num\n")
+    }
+    val sabores = listOf("cenoura", "chocolate", "maça")
+    for (sabores in sabores){
+        println("Que delicia, é um bolo de $sabores\n")
+    }
+    //While
+    val bolosComidos = 0
+    while (bolosComidos < 3){
+        println("Coma um bolo")
+    }
+}
